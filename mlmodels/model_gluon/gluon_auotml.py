e0c6b14c739833332b3d5bd56950e4dd00386cfc
arita37 <noelkev0@gmail.com>
2020-02-10 23:36:52 +0900

    autogluon

--- mlmodels/model_gluon/gluon_auotml.py
+++ mlmodels/model_gluon/gluon_auotml.py
@@ -1,16 +1,78 @@
 # -*- coding: utf-8 -*-
 """
-Gluon
+AutGluon
+
+https://autogluon.mxnet.io/tutorials/tabular_prediction/tabular-quickstart.html
+
+
 
 """
-from gluonts.trainer import Trainer
-from gluonts.model.deepar import DeepAREstimator
 
 
 
-from mlmodels.model_gluon.util import *
+from mlmodels.model_gluon.util_autogluon import *
 import os
 
+
+"""
+
+# First install package from terminal:  pip install mxnet autogluon
+
+from autogluon import TabularPrediction as task
+train_data = task.Dataset(file_path='https://autogluon.s3.amazonaws.com/datasets/Inc/train.csv')
+test_data = task.Dataset(file_path='https://autogluon.s3.amazonaws.com/datasets/Inc/test.csv')
+predictor = task.fit(train_data=train_data, label='class')
+performance = predictor.evaluate(test_data)
+
+
+
+
+
+
+import autogluon as ag
+from autogluon import TabularPrediction as task
+
+
+
+train_data = task.Dataset(file_path='https://autogluon.s3.amazonaws.com/datasets/Inc/train.csv')
+train_data = train_data.head(500) # subsample 500 data points for faster demo
+print(train_data.head())
+
+
+label_column = 'class'
+print("Summary of class variable: \n", train_data[label_column].describe())
+
+
+
+dir = 'agModels-predictClass' # specifies folder where to store trained models
+predictor = task.fit(train_data=train_data, label=label_column, output_directory=dir)
+
+
+test_data = task.Dataset(file_path='https://autogluon.s3.amazonaws.com/datasets/Inc/test.csv')
+y_test = test_data[label_column]  # values to predict
+test_data_nolab = test_data.drop(labels=[label_column],axis=1) # delete label column to prove we're not cheating
+print(test_data_nolab.head())
+
+
+from autogluon import TabularPrediction as task
+predictor = task.fit(train_data=task.Dataset(file_path=<file-name>), label_column=<variable-name>)
+
+results = predictor.fit_summary()
+
+print("AutoGluon infers problem type is: ", predictor.problem_type)
+print("AutoGluon categorized the features as: ", predictor.feature_types)
+
+
+
+
+
+
+
+
+
+
+
+"""
 ########################################################################################################################
 #### Model defintion
 class Model(object) :
